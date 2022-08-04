63ec89e2de73d652a1c098cc9c46a49e359bcf5d
sai <saikiranjagari@gmail.com>
2022-08-04 13:44:37 +0530

    Init

---
+++ src/main/java/demo/demo/UserController.java
@@ -0,0 +1,12 @@
+package demo.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController{
+      @GetMapping
+      public String demo(){
+        return "Hello world";
+    }
+}
